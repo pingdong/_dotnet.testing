f2263fc71cf932216ec4396ff940cbd089cf1c9e
Ping Dong <ping.dong@parallo.com>
2019-05-25 19:59:07 +1200

    Add the ability of the nested object compare

--- src/PingDong.Testing.UnitTests/Helper/PropertiesComparer.cs
+++ src/PingDong.Testing.UnitTests/Helper/PropertiesComparer.cs
@@ -1,31 +1,33 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using Xunit;
 
 namespace PingDong.Testing
 {
-    public class FlatObjectPropertiesComparerTests
+    public class PropertiesComparerTests
     {
         #region Sample Objct
+
         private class TestTarget
         {
             public string S { get; set; }
             public int I { get; set; }
         }
-        #endregion
 
-        [Fact]
-        public void NullCompareToObject_ShouldReturn_False()
+        private class TestTargetWrap
         {
-            string s1 = null;
-            string s2 = "A";
-            Assert.NotEqual(s1, s2, new FlatObjectPropertiesComparer<string>());
+            public TestTarget Target { get; set; }
+            
+            public string S { get; set; }
         }
 
+        #endregion
+        
         [Fact]
         public void Null_ShouldReturn_True()
         {
             string s1 = null;
             string s2 = null;
-            Assert.Equal(s1, s2, new FlatObjectPropertiesComparer<string>());
+            Assert.Equal(s1, s2, new PropertiesComparer<string>());
         }
 
         [Fact]
@@ -33,7 +35,35 @@ namespace PingDong.Testing
         {
             var s1 = new string[] { };
             var s2 = new string[] { };
-            Assert.Equal(s1, s2, new FlatObjectPropertiesComparer<string[]>());
+            Assert.Equal(s1, s2, new PropertiesComparer<string[]>());
+        }
+
+        [Fact]
+        public void NewObject_ShouldReturn_True()
+        {
+            var t1 = new TestTarget();
+            var t2 = new TestTarget();
+            Assert.Equal(t1, t2, new PropertiesComparer<TestTarget>());
+
+            var w1 = new TestTargetWrap();
+            var w2 = new TestTargetWrap();
+            Assert.Equal(w1, w2, new PropertiesComparer<TestTargetWrap>());
+        }
+
+        [Fact]
+        public void TwoListsWithSamePropertiesInstance_ShouldReturn_True()
+        {
+            var list1 = new List<TestTarget> { new TestTarget { S = "1", I = 1} };
+            var list2 = new List<TestTarget> { new TestTarget { S = "1", I = 1} };
+            Assert.Equal(list1, list2, new PropertiesComparer<TestTarget>());
+        }
+
+        [Fact]
+        public void TwoListsWithDifferentPropertiesInstance_ShouldReturn_False()
+        {
+            var list1 = new List<TestTarget> { new TestTarget { S = "1", I = 1} };
+            var list2 = new List<TestTarget> { new TestTarget { S = "1A", I = 1} };
+            Assert.NotEqual(list1, list2, new PropertiesComparer<TestTarget>());
         }
 
         [Fact]
@@ -41,7 +71,9 @@ namespace PingDong.Testing
         {
             var t1 = new TestTarget { S = "1", I = 1};
             var t2 = new TestTarget { S = "1", I = 1};
-            Assert.Equal(t1, t2, new FlatObjectPropertiesComparer<TestTarget>());
+
+            Assert.NotEqual(t1, t2);
+            Assert.Equal(t1, t2, new PropertiesComparer<TestTarget>());
         }
 
         [Fact]
@@ -49,7 +81,7 @@ namespace PingDong.Testing
         {
             var t1 = new TestTarget { S = "1", I = 1};
             var t2 = new TestTarget { S = "1A", I = 1};
-            Assert.NotEqual(t1, t2, new FlatObjectPropertiesComparer<TestTarget>());
+            Assert.NotEqual(t1, t2, new PropertiesComparer<TestTarget>());
         }
 
         [Fact]
@@ -57,7 +89,34 @@ namespace PingDong.Testing
         {
             var t1 = new TestTarget { S = "1", I = 1};
             var t2 = t1;
-            Assert.Equal(t1, t2, new FlatObjectPropertiesComparer<TestTarget>());
+
+            Assert.Equal(t1, t2);
+            Assert.Equal(t1, t2, new PropertiesComparer<TestTarget>());
+        }
+
+        [Fact]
+        public void TwoNestedInstanceWithSameProperties_ShouldReturn_True()
+        {
+            var t1 = new TestTargetWrap {Target = new TestTarget {S = "1", I = 1}, S = "S"};
+            var t2 = new TestTargetWrap {Target = new TestTarget {S = "1", I = 1}, S = "S"};
+
+            Assert.Equal(t1, t2, new PropertiesComparer<TestTargetWrap>());
+        }
+
+        [Fact]
+        public void TwoNestedInstanceWithDifferentProperties_ShouldReturn_False()
+        {
+            var t1 = new TestTargetWrap {Target = new TestTarget {S = "1", I = 1}, S = "S"};
+            var t2 = new TestTargetWrap {Target = new TestTarget {S = "1A", I = 1}, S = "S"};
+            Assert.NotEqual(t1, t2, new PropertiesComparer<TestTargetWrap>());
+        }
+
+        [Fact]
+        public void TwoNestedInstanceOfTheSameObject_ShouldReturn_True()
+        {
+            var t1 = new TestTargetWrap {Target = new TestTarget {S = "1", I = 1}, S = "S"};
+            var t2 = t1;
+            Assert.Equal(t1, t2, new PropertiesComparer<TestTargetWrap>());
         }
     }
 }
